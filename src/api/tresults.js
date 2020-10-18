89f404a40af50a6d10d5f83039ba69ee629a2dd6
허만갑 <aksrkq93@naver.com>
2020-10-18 18:14:16 +0900

    20201018 1815 Update api sending task time data

--- src/api/tresults.js
+++ src/api/tresults.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { sequelize, Sequelize } = require('../../models');
 const models = require('../../models');
 
 // router init
@@ -27,19 +28,27 @@ router.post('/', async (req, res) => {
 
 router.post('/tkid', async (req, res) => {
     try {
-        const doc = await models.tresult.findAll({
-            attributes: ['tkid', 'etime', 'rtime', 'cdatetime'],
-            where: {
-                ptid: req.body.ptid,
-                ctid: req.body.ctid,
-                tkid: req.body.tkid,
-                // cdatetime: {
-                //     $lte: req.body.cdatetime,
-                // }
-            },
-            order: [['cdatetime', 'desc']],
-            // limit: 30,
-        });
+        let ptid = req.body.ptid;
+        let ctid = req.body.ctid;
+        let tkidCdateArr = req.body.tkidCdateArr;
+
+        let query = `(SELECT tkid, etime, rtime, cdatetime FROM tresult
+                        WHERE ptid = ${ptid} AND ctid = ${ctid} AND tkid = ${tkidCdateArr[0].tkid} AND cdatetime <= ${tkidCdateArr[0].cdatetime}
+                        ORDER BY cdatetime DESC LIMIT 30)`;
+
+        for (let i = 1; i < tkidCdateArr.length; i++) {
+            query += ` UNION ALL
+                    (SELECT tkid, etime, rtime, cdatetime FROM tresult
+                        WHERE ptid = ${ptid} AND ctid = ${ctid} AND tkid = ${tkidCdateArr[i].tkid} AND cdatetime <= ${tkidCdateArr[i].cdatetime}
+                        ORDER BY cdatetime DESC LIMIT 30)`
+        }
+
+        const doc = await sequelize.query(
+                query,
+            {
+                type: Sequelize.QueryTypes.SELECT,
+            }
+        );
         console.log('doc', doc);
         res.status(200).json({
             taskTimes: doc,
