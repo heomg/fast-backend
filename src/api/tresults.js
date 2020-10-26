492e316818fba0356ca79ab9e35bb436fe63e7a3
허만갑 <aksrkq93@naver.com>
2020-10-26 16:18:06 +0900

    20201026 1618 Add api sending joint history data

--- src/api/tresults.js
+++ src/api/tresults.js
@@ -7,7 +7,6 @@ const router = Router();
 
 router.post('/', async (req, res) => {
     res.set({ 'access-control-allow-origin': '*' });
-    console.log('abc');
     try {
         const doc = await models.tresult.findAll({
             where: {
@@ -55,6 +54,63 @@ router.post('/tkid', async (req, res) => {
         });
     } catch (error) {
         res.status(400).json({ message: 'sth wrong', error });
+    }
+});
+
+router.post('/handData', async (req, res) => {
+    try {
+        let ptid = req.body.ptid;
+        let ctid = req.body.ctid;
+        let tkidCdateArr = req.body.tkidCdateArr;
+
+        let query = `(SELECT tkid, cdatetime,
+                             lthumbfirstmin, lthumbfirstmax, lthumbsecondmin, lthumbsecondmax,
+                             lindexfirstmin, lindexfirstmax, lindexsecondmin, lindexsecondmax,
+                             lmiddlefirstmin, lmiddlefirstmax, lmiddlesecondmin, lmiddlesecondmax,
+                             lringfirstmin, lringfirstmax, lringsecondmin, lringsecondmax,
+                             lpinkyfirstmin, lpinkyfirstmax, lpinkysecondmin, lpinkysecondmax, 
+                             rthumbfirstmin, rthumbfirstmax, rthumbsecondmin, rthumbsecondmax,
+                             rindexfirstmin, rindexfirstmax, rindexsecondmin, rindexsecondmax,
+                             rmiddlefirstmin, rmiddlefirstmax, rmiddlesecondmin, rmiddlesecondmax,
+                             rringfirstmin, rringfirstmax, rringsecondmin, rringsecondmax,
+                             rpinkyfirstmin, rpinkyfirstmax, rpinkysecondmin, rpinkysecondmax
+                        FROM tresult
+                        WHERE ptid = ${ptid} AND ctid = ${ctid} AND tkid = ${tkidCdateArr[0].tkid} AND cdatetime <= ${tkidCdateArr[0].cdatetime}
+                        ORDER BY cdatetime DESC LIMIT 30)`;
+        
+        for (let i = 1; i < tkidCdateArr.length; i++) {
+            query += ` UNION ALL
+                    (SELECT tkid, cdatetime,
+                            lthumbfirstmin, lthumbfirstmax, lthumbsecondmin, lthumbsecondmax,
+                            lindexfirstmin, lindexfirstmax, lindexsecondmin, lindexsecondmax,
+                            lmiddlefirstmin, lmiddlefirstmax, lmiddlesecondmin, lmiddlesecondmax,
+                            lringfirstmin, lringfirstmax, lringsecondmin, lringsecondmax,
+                            lpinkyfirstmin, lpinkyfirstmax, lpinkysecondmin, lpinkysecondmax, 
+                            rthumbfirstmin, rthumbfirstmax, rthumbsecondmin, rthumbsecondmax,
+                            rindexfirstmin, rindexfirstmax, rindexsecondmin, rindexsecondmax,
+                            rmiddlefirstmin, rmiddlefirstmax, rmiddlesecondmin, rmiddlesecondmax,
+                            rringfirstmin, rringfirstmax, rringsecondmin, rringsecondmax,
+                            rpinkyfirstmin, rpinkyfirstmax, rpinkysecondmin, rpinkysecondmax
+                        FROM tresult
+                        WHERE ptid = ${ptid} AND ctid = ${ctid} AND tkid = ${tkidCdateArr[i].tkid} AND cdatetime <= ${tkidCdateArr[i].cdatetime}
+                        ORDER BY cdatetime DESC LIMIT 30)`
+        }
+
+        const doc = await sequelize.query(
+            query,
+            {
+                type: Sequelize.QueryTypes.SELECT,
+            }
+        );
+
+        console.log(doc);
+
+        res.status(200).json({
+            handData: doc,
+        });
+
+    } catch (error) {
+        res.status(400).json({ message: 'handData error', error });
     }
 });
 
