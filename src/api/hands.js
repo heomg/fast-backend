0fe77ec15cd91cd0fe0b0d350d8f56c5159df21d
허만갑 <aksrkq93@naver.com>
2020-11-16 13:55:12 +0900

    20201116 1355 Add hands api for hands graph

--- src/api/hands.js
+++ src/api/hands.js
@@ -3,9 +3,19 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+    try {
+        console.log('req.body', req.body);
+        res.send('hello');
+    } catch (error) {
+        res.status(400).json({ message: 'sth wrong', error })
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         console.log('req.body', req.body);
+        res.send('hello');
     } catch (error) {
         res.status(400).json({ message: 'sth wrong', error })
     }
